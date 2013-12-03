57ea917968973b37310771f1b93fe01688b9f8a6
herbert <herbert_shmh@163.com>
2013-12-03 15:08:28 +0800

    修改：      app/source/main.c

--- app/source/main.c
+++ app/source/main.c
@@ -5,13 +5,13 @@
 
 int main (int argc, char *argv[])
 {
-#if 1
     int result;
     result = add (3, 5);
     printf ("add: result = %d\n", result);
 
     result = mul (3, 5);
     printf ("mul: result = %d\n", result);
-#endif
+    
+
     return 0;
 }
